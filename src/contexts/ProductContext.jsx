3c60003c1e9fc2aa321b26c7db8c7247769cb7ef
Michael Agyemang Prempeh <agyemangmichael555@gmail.com>
2024-10-21 17:31:35 +0000

    after class

--- src/contexts/ProductContext.jsx
+++ src/contexts/ProductContext.jsx
@@ -4,23 +4,40 @@ import { useParams } from "react-router-dom";
 const ProductContext = createContext();
 
 export const ProductProvider = () => {
-    const [product, setProduct] = useState([]);
+    const [products, setProducts] = useState([]);
     const [loading, setLoading] = useState(false);
-    
+
     const { category } = useParams();
 
-    const getProducts = async () => {
+    const getProducts = async (category) => {
         try {
-            let url='#'
+            setLoading(true)
+            let url = 'https://fakestoreapi.com/products';
+            if (category) {
+                url += `/category/${category}/?limit=4`;
+            } else {
+                url += `?limit=4`
+            }
+            const response = await fetch(url);
+            if (response.ok) {
+                setLoading(false)
+                setProducts(data);
+            }
         } catch (error) {
-            
+            console.log(error);
+        } finally {
+            setLoading(false);
         }
-    }
+    };
 
-    return
-    <ProductContext.Provider value={{}}>
-        ProductContext
-    </ProductContext.Provider>
+    useEffect(() => {
+        getProducts(category)
+    }, [category])
+
+    return (
+        <ProductContext.Provider value={{ products, loading }}>
+            ProductContext
+        </ProductContext.Provider>)
 }
 
 export const useProduct = () => useContext(ProductContext)
